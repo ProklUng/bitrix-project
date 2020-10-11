42c70b45cac29af6da677b07e04b81aad0210f72
Fedor Kavkazcky <funtik21975@gmail.com>
2020-10-11 10:41:45 +0300

    Обновление.

--- local/classes/Services/Bitrix/GetApplication.php
+++ local/classes/Services/Bitrix/GetApplication.php
@@ -6,7 +6,10 @@ use CMain;
 
 /**
  * Class GetApplication
+ * $APPLICATION для инжекции в сервисы и контроллеры.
  * @package Local\Services\Bitrix
+ *
+ * @since 11.10.2020
  */
 class GetApplication
 {
