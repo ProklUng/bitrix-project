efcdc8356786ce85dbda128f865928e04387be35
Fedor <funtik21975@gmail.com>
2020-11-08 10:05:34 +0300

    Доработка Assets.

--- local/classes/Util/Assets.php
+++ local/classes/Util/Assets.php
@@ -3,12 +3,16 @@
 namespace Local\Util;
 
 use Exception;
+use InvalidArgumentException;
+use RuntimeException;
 
 /**
  * Class Assets
  * @package Local\Util
  *
- * Хелпер для работы с манифест-файлом Вебпака
+ * Хелпер для работы с манифест-файлом Вебпака.
+ *
+ * @since 08.11.2020 Получение entrypoints.
  */
 class Assets
 {
@@ -16,14 +20,21 @@ class Assets
      * @var string
      */
     private $base;
+
     /**
      * @var string
      */
     private $manifestFile;
+
     /**
      * @var array
      */
     private $manifest;
+
+    /**
+     * @var array $entrypoints Содержимое entrypoints.json.
+     */
+    private $entrypoints;
 
     /**
      * Assets constructor.
@@ -78,5 +89,83 @@ class Assets
         }
 
         return $entry;
+    }
+
+    /**
+     * Данные на entrypoint js из entrypoints.js.
+     *
+     * @param string $entrypoint Entrypoint.
+     *
+     * @return array
+     *
+     * @throws InvalidArgumentException
+     *
+     * @since 08.11.2020
+     */
+    public function getWebpackJsFiles($entrypoint)
+    {
+        if (!array_key_exists($entrypoint, $this->getEntrypoints())) {
+            throw new InvalidArgumentException(sprintf('Invalid entrypoint "%s"', $entrypoint));
+        }
+
+        $files = $this->getEntrypoints()[$entrypoint];
+        if (!array_key_exists('js', $files)) {
+            return [];
+        }
+
+        return $files['js'];
+    }
+
+    /**
+     * Данные на entrypoint css из entrypoints.js.
+     *
+     * @param string $entrypoint Entrypoint.
+     *
+     * @return array
+     *
+     * @throws InvalidArgumentException
+     *
+     * @since 08.11.2020
+     */
+    public function getWebpackCssFiles($entrypoint)
+    {
+        if (!array_key_exists($entrypoint, $this->getEntrypoints())) {
+            throw new InvalidArgumentException(sprintf('Invalid entrypoint "%s"', $entrypoint));
+        }
+
+        $files = $this->getEntrypoints()[$entrypoint];
+        if (!array_key_exists('css', $files)) {
+            return [];
+        }
+
+        return $files['css'];
+    }
+
+    /**
+     * Entrypoints.
+     *
+     * @return array
+     * @throws RuntimeException Ошибки чтения и декодирования entrypoints.json.
+     *
+     * @since 08.11.2020
+     */
+    private function getEntrypoints(): array
+    {
+        if (null === $this->entrypoints) {
+            $file = $this->base . DIRECTORY_SEPARATOR . 'entrypoints.json';
+            $content = file_get_contents($file);
+            if ($content === false) {
+                throw new RuntimeException(\sprintf('Unable to read file "%s"', $file));
+            }
+
+            $json = json_decode($content, true);
+            if (JSON_ERROR_NONE !== json_last_error()) {
+                throw new RuntimeException(\sprintf('Unable to decode json file "%s"', $file));
+            }
+
+            $this->entrypoints = $json['entrypoints'];
+        }
+
+        return $this->entrypoints;
     }
 }
