04070210a8d8da73c2dfebb9a4d801382f6d0430
Fedor <funtik21975@gmail.com>
2020-11-15 23:01:02 +0300

    GuzzleBundle & подключение MonologBundle.

---
+++ local/classes/Bundles/GuzzleBundle/Middlewares/Cache/Adapter/StorageAdapterInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the CsaGuzzleBundle package
+ *
+ * (c) Charles Sarrazin <charles@sarraz.in>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ */
+
+namespace Local\Bundles\GuzzleBundle\Middlewares\Cache\Adapter;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+interface StorageAdapterInterface
+{
+    /**
+     * @param RequestInterface $request
+     *
+     * @return null|ResponseInterface
+     */
+    public function fetch(RequestInterface $request);
+
+    /**
+     * @param RequestInterface  $request
+     * @param ResponseInterface $response
+     *
+     * @return void
+     */
+    public function save(RequestInterface $request, ResponseInterface $response);
+}
--- local/configs/standalone_bundles.php
+++ local/configs/standalone_bundles.php
@@ -1,5 +1,7 @@
 <?php
 return [
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Local\Bundles\ApiExceptionBundle\M6WebApiExceptionBundle::class => ['all' => true],
     // Local\Bundles\ApiDtoConvertorBundle\ApiDtoConvertorBundle::class => ['all' => true],
+    Local\Bundles\GuzzleBundle\CsaGuzzleBundle::class => ['all' => true],
 ];
