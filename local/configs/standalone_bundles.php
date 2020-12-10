fab57a34c75fddd1efe1037ef990d4668465d5a6
Fedor Kavkazcky <funtik21975@gmail.com>
2020-12-10 16:47:22 +0300

    TaskSchedulerBundle.

--- local/configs/standalone_bundles.php
+++ local/configs/standalone_bundles.php
@@ -5,4 +5,6 @@ return [
     Local\Bundles\ApiExceptionBundle\M6WebApiExceptionBundle::class => ['all' => true],
     // Local\Bundles\ApiDtoConvertorBundle\ApiDtoConvertorBundle::class => ['all' => true],
     Local\Bundles\GuzzleBundle\CsaGuzzleBundle::class => ['all' => true],
-    Local\Bundles\SymfonyMiddlewareBundle\MiddlewareBundle::class => ['all' => true],];
+    Local\Bundles\SymfonyMiddlewareBundle\MiddlewareBundle::class => ['all' => true],
+    Local\Bundles\TaskSchedulerBundle\RewieerTaskSchedulerBundle::class => ['all' => true],
+    ];
