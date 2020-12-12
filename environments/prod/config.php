b2f1297c5894a574a5ed39680ad89a8ea441559d
Fedor Kavkazcky <funtik21975@gmail.com>
2020-12-12 11:55:18 +0300

    Локальный (и упрощенный) форк Console Jedi. Выпилены не нужные функции. Устранена внешняя зависимость от устаревшей версии Symfony.

--- environments/prod/config.php
+++ environments/prod/config.php
@@ -1,27 +1,27 @@
 <?php
 /**
  * Example of settings file config.php:
- *
+ * 
  * ```php
  * return [
  *      // License key for Bitrix
  *      'licenseKey' => 'NFR-123-456',
- *
+ * 
  *      // Modules to be installed.
- *      // Warning: install the modules using DB migration. Install the modules
+ *      // Warning: install the modules using DB migration. Install the modules 
  *      // using the settings of the environment, only for dev environment.
  *      'modules' => [
  *          'vendor.debug'
  *      ],
- *
- *      // Options for modules
+ * 
+ *      // Options for modules 
  *      'options' => [
  *          'vendor.module' => [
  *              'OPTION_CODE' => 'value',
  *              'OPTION_CODE' => ['value' => 'test', 'siteId' => 's1']
  *          ],
  *      ],
- *
+ * 
  *      // Settings for module "cluster"
  *      'cluster' => [
  *          'memcache' => [
@@ -41,7 +41,7 @@
  *              ]
  *          ]
  *      ],
- *
+ * 
  *      // Values for file .settings.php
  *      'settings' => [
  *          'connections' => [
@@ -59,12 +59,13 @@
  * ```
  */
 
+
 return [
     // License key for Bitrix
     'licenseKey' => 'DEMO',
     'options' => [
         'main' => [
-            'site_name' => 'Ð˜Ð¼Ñ ÑÐ°Ð¹Ñ‚Ð°',
+            'site_name' => 'Èìÿ ñàéòà',
             'server_name' => 'site_url',
             'error_reporting' => 0,
             'optimize_css_files' => 'Y',
@@ -76,24 +77,24 @@ return [
             'map_left_menu_type' => 'left',
         ],
         'update' => [
-            'update_devsrv' => 'Y',
+            'update_devsrv' => 'N',
         ],
         'event_log' => [
-            "event_log_logout" => 'N',
-            "event_log_login_success" => 'N',
-            "event_log_login_fail" => 'N',
-            "event_log_register" => 'N',
-            "event_log_register_fail" => 'N',
-            "event_log_password_request" => 'N',
-            "event_log_password_change" => 'N',
-            "event_log_user_edit" => 'N',
-            "event_log_user_delete" => 'N',
-            "event_log_user_groups" => 'N',
-            "event_log_group_policy" => 'N',
-            "event_log_module_access" => 'N',
-            "event_log_file_access" => 'N',
-            "event_log_task" => 'N',
-            "event_log_marketplace" => 'N',
+            "event_log_logout" => 'Y',
+            "event_log_login_success" => 'Y',
+            "event_log_login_fail" => 'Y',
+            "event_log_register" => 'Y',
+            "event_log_register_fail" => 'Y',
+            "event_log_password_request" => 'Y',
+            "event_log_password_change" => 'Y',
+            "event_log_user_edit" => 'Y',
+            "event_log_user_delete" => 'Y',
+            "event_log_user_groups" => 'Y',
+            "event_log_group_policy" => 'Y',
+            "event_log_module_access" => 'Y',
+            "event_log_file_access" => 'Y',
+            "event_log_task" => 'Y',
+            "event_log_marketplace" => 'Y',
         ]
     ],
 ];
--- local/classes/ConsoleJedi/tmpl/environments/default/config.php
+++ local/classes/ConsoleJedi/tmpl/environments/default/config.php
@@ -60,40 +60,4 @@
  */
 
 return [
-    // License key for Bitrix
-    'licenseKey' => 'DEMO',
-    'options' => [
-        'main' => [
-            'site_name' => 'Имя сайта',
-            'server_name' => 'site_url',
-            'error_reporting' => 0,
-            'optimize_css_files' => 'Y',
-            'optimize_js_files' => 'Y',
-            'use_minified_assets' => 'Y',
-            'move_js_to_body' => 'Y',
-            'compres_css_js_files' => 'Y',
-            'map_top_menu_type' => 'top',
-            'map_left_menu_type' => 'left',
-        ],
-        'update' => [
-            'update_devsrv' => 'Y',
-        ],
-        'event_log' => [
-            "event_log_logout" => 'N',
-            "event_log_login_success" => 'N',
-            "event_log_login_fail" => 'N',
-            "event_log_register" => 'N',
-            "event_log_register_fail" => 'N',
-            "event_log_password_request" => 'N',
-            "event_log_password_change" => 'N',
-            "event_log_user_edit" => 'N',
-            "event_log_user_delete" => 'N',
-            "event_log_user_groups" => 'N',
-            "event_log_group_policy" => 'N',
-            "event_log_module_access" => 'N',
-            "event_log_file_access" => 'N',
-            "event_log_task" => 'N',
-            "event_log_marketplace" => 'N',
-        ]
-    ],
 ];
