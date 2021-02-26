34d3b4a7c0d4326dc29fc3a39f910dcd6ef5dbd4
Fedor <funtik21975@gmail.com>
2021-02-26 13:59:53 +0300

    DtoMapperBundle. AutoMapperPlus -> DTO.

--- local/configs/standalone_bundles.php
+++ local/configs/standalone_bundles.php
@@ -17,6 +17,7 @@ return [
     Local\Bundles\BitrixIblockElementValidatorBundle\BitrixIblockElementValidatorBundle::class => ['all' => true],
     Local\Bundles\UrlSignedBundle\UrlSignedBundle::class => ['all' => true],
     Local\Bundles\BitrixOgGraphBundle\BitrixOgGraphBundle::class => ['all' => true],
-    Local\Bundles\InstagramParserRapidApiBundle\InstagramParserRapidApiBundle::class => ['all' => true]
-
+    Local\Bundles\InstagramParserRapidApiBundle\InstagramParserRapidApiBundle::class => ['all' => true],
+    AutoMapperPlus\AutoMapperPlusBundle\AutoMapperPlusBundle::class => ['all' => true],
+    Local\Bundles\DtoMapperBundle\DtoMapperBundle::class => ['all' => true],
 ];
