b25a41fc12ab486ff80f99c757e3b3050ec646da
Fedor <funtik21975@gmail.com>
2021-02-26 08:49:05 +0300

    ConsoleCommandConfigurator. Доработка.

--- local/classes/Services/Console/ConsoleCommandConfigurator.php
+++ local/classes/Services/Console/ConsoleCommandConfigurator.php
@@ -3,9 +3,11 @@
 namespace Local\Services\Console;
 
 use Exception;
+use IteratorAggregate;
 use Local\ServiceProvider\Bundles\BundlesLoader;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\CommandNotFoundException;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -15,6 +17,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  *
  * @since 10.12.2020
  * @since 20.12.2020 Рефакторинг. Форк нативного способа подключения команд.
+ * @since 26.02.2021 Убрал array_merge в цикле.
  */
 class ConsoleCommandConfigurator
 {
@@ -73,17 +76,43 @@ class ConsoleCommandConfigurator
     /**
      * Добавить команды.
      *
-     * @param array $commands Команды
+     * @param array $commands Команды.
      *
      * @return void
+     * @throws Exception
      */
     public function add(...$commands): void
     {
+        $result = [];
+
         foreach ($commands as $command) {
-            $iterator = $command->getIterator();
-            $array = iterator_to_array($iterator);
-            $this->commands = array_merge($this->commands, $array);
+            $array = $command;
+            if ($command instanceof IteratorAggregate) {
+                $iterator = $command->getIterator();
+                $array = iterator_to_array($iterator);
+            }
+
+            $result[] = $array;
         }
+
+        $this->commands = array_merge($this->commands, $result);
+    }
+
+    /**
+     * Finds a command by name or alias.
+     *
+     * Contrary to get, this command tries to find the best
+     * match if you give it an abbreviation of a name or alias.
+     *
+     * @param string $name A command name or a command alias.
+     *
+     * @return Command A Command instance
+     *
+     * @throws CommandNotFoundException When command name is incorrect or ambiguous
+     */
+    public function find(string $name) : Command
+    {
+        return $this->application->find($name);
     }
 
     /**
