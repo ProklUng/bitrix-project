f5d86125d9cc9511e347b6ae0c338c548124c942
Fedor <funtik21975@gmail.com>
2021-03-08 22:09:51 +0300

    SymfonyBladeBundle. Поддержка шаблонизатора Blade - как в компонентах, так и в качестве отдельного рендерера.

---
+++ local/classes/Bundles/SymfonyBladeBundle/Services/ViewFinder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Local\Bundles\SymfonyBladeBundle\Services;
+
+use Illuminate\View\FileViewFinder;
+
+/**
+ * Class ViewFinder
+ * @package Local\Bundles\SymfonyBladeBundle\Services
+ */
+class ViewFinder extends FileViewFinder
+{
+    /**
+     * Setter for paths.
+     *
+     * @param array $paths Пути.
+     *
+     * @return void
+     */
+    public function setPaths(array $paths) : void
+    {
+        $this->paths = $paths;
+    }
+}
