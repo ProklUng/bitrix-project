05ab0e920088bb3d8bfb4f9288165f1b972a3a3c
Fedor <funtik21975@gmail.com>
2021-03-09 14:32:27 +0300

    SymfonyBladeBundle. Refactoring BladeCacheClearer.

--- local/classes/Bundles/SymfonyBladeBundle/Services/Utils/BladeCacheClearer.php
+++ local/classes/Bundles/SymfonyBladeBundle/Services/Utils/BladeCacheClearer.php
@@ -3,6 +3,7 @@
 namespace Local\Bundles\SymfonyBladeBundle\Services\Utils;
 
 use Bitrix\Main\Application;
+use Local\Bundles\SymfonyBladeBundle\Services\Providers\BladeProvider;
 
 /**
  * Class BladeCacheClearer
@@ -13,55 +14,19 @@ use Bitrix\Main\Application;
 class BladeCacheClearer
 {
     /**
-     * @var string $cachePath Путь к кэшу.
-     */
-    private $cachePath;
-
-    /**
-     * BladeCacheClearer constructor.
-     *
-     * @param string $cachePath Путь к кэшу.
-     */
-    public function __construct(string $cachePath)
-    {
-        $this->cachePath = $cachePath;
-    }
-
-    /**
      * Движуха.
+     *
      * @return void
      */
     public function clear() : void
     {
         $request = Application::getInstance()->getContext()->getRequest();
         $clearCache = htmlspecialchars($request->getQuery('clear_cache'));
-        if ($clearCache === 'Y') {
-            $this->rmdirRecursive($this->cachePath);
-        }
-    }
 
-    /**
-     * Рекурсивно удалить директорию.
-     *
-     * @param string $dir Директория.
-     *
-     * @return void
-     */
-    private function rmdirRecursive(string $dir) : void
-    {
-        if (is_dir($dir)) {
-            $objects = scandir($dir);
-            foreach ($objects as $object) {
-                if ($object !== '.' && $object !== '..') {
-                    if (is_dir($dir.DIRECTORY_SEPARATOR.$object)) {
-                        $this->rmdirRecursive($dir.DIRECTORY_SEPARATOR.$object);
-                    } else {
-                        unlink($dir.DIRECTORY_SEPARATOR.$object);
-                    }
-                }
-            }
-            reset($objects);
-            rmdir($dir);
+        if ($clearCache === 'Y') {
+            try {
+                BladeProvider::clearCache();
+            } catch (\Exception $e) {}
         }
     }
 }
