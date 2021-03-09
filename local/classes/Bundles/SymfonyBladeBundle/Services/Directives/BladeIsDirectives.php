4943ae60163c280d8e67c27ba93978e911623d0a
Fedor <funtik21975@gmail.com>
2021-03-09 09:21:47 +0300

    SymfonyBladeBundle. Поддержка регистрации кастомных директив.

--- local/classes/Bundles/SymfonyBladeBundle/Services/Directives/BladeIsDirectives.php
+++ local/classes/Bundles/SymfonyBladeBundle/Services/Directives/BladeIsDirectives.php
@@ -20,6 +20,16 @@ class BladeIsDirectives implements BladeDirectiveInterface
     public function handlers(BladeCompiler $compiler) : array
     {
         return [
+            /*
+            |---------------------------------------------------------------------
+            | @istrue / @isfalse
+            |---------------------------------------------------------------------
+            |
+            | These directives can be used in different ways.
+            | @istrue($v) Echo this @endistrue, @istrue($v, 'Echo this')
+            | or @istrue($variable, $echoThisVariables)
+            |
+            */
             'istrue' => function ($expression) {
                 if (strpos($expression, ',') !== false) {
                     $expression = Parser::multipleArgs($expression);
@@ -53,6 +63,53 @@ class BladeIsDirectives implements BladeDirectiveInterface
             },
 
             'endisfalse' => function ($expression) {
+                return '<?php endif; ?>';
+            },
+
+            /*
+            |---------------------------------------------------------------------
+            | @isnull / @isnotnull
+            |---------------------------------------------------------------------
+            |
+            | These directives can be used in different ways.
+            | @isnull($v) Echo this @endisnull, @isnull($v, 'Echo this')
+            | or @isnull($variable, $echoThisVariables)
+            |
+            */
+
+            'isnull' => function ($expression) {
+                if (strpos($expression, ',') !== false) {
+                    $expression = Parser::multipleArgs($expression);
+
+                    return implode('', [
+                        "<?php if (is_null({$expression->get(0)})) : ?>",
+                        "<?php echo {$expression->get(1)}; ?>",
+                        '<?php endif; ?>',
+                    ]);
+                }
+
+                return "<?php if (is_null({$expression})) : ?>";
+            },
+
+            'endisnull' => function ($expression) {
+                return '<?php endif; ?>';
+            },
+
+            'isnotnull' => function ($expression) {
+                if (strpos($expression, ',') !== false) {
+                    $expression = Parser::multipleArgs($expression);
+
+                    return implode('', [
+                        "<?php if (! is_null({$expression->get(0)})) : ?>",
+                        "<?php echo {$expression->get(1)}; ?>",
+                        '<?php endif; ?>',
+                    ]);
+                }
+
+                return "<?php if (! is_null({$expression})) : ?>";
+            },
+
+            'endisnotnull' => function ($expression) {
                 return '<?php endif; ?>';
             },
         ];
