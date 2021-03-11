7f07b2e09deb3e6ebf2c56aa2f181d6367583649
Fedor <funtik21975@gmail.com>
2021-03-11 16:51:28 +0300

    BitrixUtilsBundle.

--- local/classes/Bundles/BitrixUtilsBundle/Services/ModuleInitializer.php
+++ local/classes/Bundles/BitrixUtilsBundle/Services/ModuleInitializer.php
@@ -2,9 +2,9 @@
 
 namespace Local\Bundles\BitrixUtilsBundle\Services;
 
-use Bitrix\Main\Loader;
 use Bitrix\Main\LoaderException;
 use Bitrix\Main\ModuleManager;
+use CModule;
 use RuntimeException;
 
 /**
@@ -28,6 +28,7 @@ class ModuleInitializer
     public function __construct(array $modules = [])
     {
         $this->modules = $modules;
+        define('MODULE_INIT_CLI', true); // Признак - запускаем не из админки.
     }
 
     /**
@@ -57,14 +58,95 @@ class ModuleInitializer
             return true;
         }
 
-        ModuleManager::registerModule($moduleId);
+        ob_start();
 
-        if (!Loader::includeModule($moduleId)) {
+        $objModuleInitializer = $this->loadModuleClass($moduleId);
+        if (!is_object($objModuleInitializer)) {
+            return false;
+        }
+
+        $objModuleInitializer->DoInstall();
+        ob_clean();
+
+        return true;
+    }
+
+    /**
+     * Удалить модуль.
+     *
+     * @param string $moduleId ID модуля.
+     *
+     * @return boolean
+     */
+    public function delete(string $moduleId) : bool
+    {
+        if (!ModuleManager::isModuleInstalled($moduleId)) {
+            return true;
+        }
+
+        ob_start();
+
+        $objModuleInitializer = $this->loadModuleClass($moduleId);
+        if (!is_object($objModuleInitializer)) {
+            return false;
+        }
+
+        $objModuleInitializer->DoUninstall();
+
+        ob_end_clean();
+
+        return true;
+    }
+
+    /**
+     * @param array $modules Модули.
+     *
+     * @return ModuleInitializer
+     */
+    public function setModules(array $modules): ModuleInitializer
+    {
+        $this->modules = $modules;
+
+        return $this;
+    }
+
+    /**
+     * Инициализировать класс модуля.
+     *
+     * @param string $moduleId ID модуля.
+     *
+     * @return false|mixed
+     * @throws RuntimeException
+     */
+    private function loadModuleClass(string $moduleId)
+    {
+        $modulePath = getLocalPath('modules/' . $moduleId . '/install/index.php');
+        $langPath = getLocalPath('modules/' . $moduleId . '/lang');
+
+        if ($modulePath === false) {
             throw new RuntimeException(
-                'Инициализация модуля .' . $moduleId . ' не задалась. Подключите его в админке.'
+                'Модуль .' . $moduleId . ' не найден.'
             );
         }
 
-        return true;
+        /** @noinspection PhpIncludeInspection */
+        require_once $_SERVER['DOCUMENT_ROOT'] . $modulePath;
+
+        $classModule = str_replace('.', '_', $moduleId);
+
+        if (!class_exists($classModule)
+            ||
+            in_array(CModule::class, class_parents($classModule), true) === false
+        ) {
+            return false;
+        }
+
+        // Языковый файл модулей по умолчанию.
+        include($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/lang/ru/admin/partner_modules.php');
+        if ($langPath) {
+            include($_SERVER['DOCUMENT_ROOT'] . $langPath . '/ru/install/install.php');
+        }
+
+        return new $classModule;
     }
 }
